6580e321dc8aa9db88500fccdbc97e03f2b88455
Chaloulou75 <c.jeandey@gmail.com>
2020-09-25 22:30:15 -0300

    contact2

--- components/Contact.vue
+++ components/Contact.vue
@@ -17,13 +17,13 @@
         <div class="flex justify-start w-full px-2 text-gray-700 lg:px:8 lg:justify-center lg:w-1/2">
           <div class="px-12 py-4 my-4 space-y-3 rounded-lg lg:my-0">
 
-            <p class="flex items-center text-base lg:text-2xl lg:my-0"><svg class="inline-block w-6 h-6 mr-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"></path></svg> +33 1 22 22 22 22</p>
+            <p class="flex items-center text-base lg:text-lg lg:my-0"><svg class="inline-block w-6 h-6 mr-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"></path></svg> +33 1 22 22 22 22</p>
 
-            <p class="flex items-center text-base lg:text-2xl"><svg class="inline-block w-6 h-6 mr-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"></path></svg>
+            <p class="flex items-center text-base lg:text-lg"><svg class="inline-block w-6 h-6 mr-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"></path></svg>
               email@email.com
             </p>
-            <p class="flex items-center text-base lg:text-2xl">
-              <span class="inline-block w-6 h-6 mr-4 font-semibold"> Fax: </span> 012345678
+            <p class="flex items-center text-base lg:text-lg">
+              <span class="mr-4 font-semibold"> Fax: </span> 012345678
             </p>
           </div>
         </div>
