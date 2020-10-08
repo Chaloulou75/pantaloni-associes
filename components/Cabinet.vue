5b250a390b1f217676292e11f9f90b7fa0b51df4
Chaloulou75 <c.jeandey@gmail.com>
2020-10-08 20:20:38 -0300

    text xl cabinet

--- components/Cabinet.vue
+++ components/Cabinet.vue
@@ -8,13 +8,16 @@
         <div class="w-full px-4 py-4 lg:w-1/2">
           <div class="my-8 line-1"></div>
           <!-- <h3 class="py-2 text-2xl font-medium text-center">Un sous titre</h3> -->
-          <p class="mx-auto text-lg tracking-wide whitespace-pre-wrap">
-          Depuis sa constitution, le Cabinet PANTALONI & ASSOCIES propose à ses clients un accompagnement sur mesure dans ses domaines d’interventions.
-
+          <p class="py-2 mx-auto text-xl tracking-wide text-justify text-indent">
+          Depuis sa constitution, le Cabinet PANTALONI &amp; ASSOCIES propose à ses clients un accompagnement sur mesure dans ses domaines d’interventions.
+          </p>
+          <p class="py-2 mx-auto text-xl tracking-wide text-justify text-indent">
           Le Cabinet dispose d’une compétence reconnue en matière pénale, en Droit de la construction, en Droit de la responsabilité et en Droit des assurances.
-
+          </p>
+          <p class="py-2 mx-auto text-xl tracking-wide text-justify text-indent">
           De taille humaine, croisant les compétences, le Cabinet assure un service personnalisé à ses clients qu’il conseille et accompagne en demande comme en défense dans le respect des règles professionnelles et notamment de confidentialité.
-
+          </p>
+          <p class="py-2 mx-auto text-xl tracking-wide text-justify text-indent">
           Sa clientèle est constituée d’Institutionnels, de grandes sociétés, moyennes et petites entreprises ainsi que de particuliers.
           </p>
           <div class="my-8 line-1"></div>
