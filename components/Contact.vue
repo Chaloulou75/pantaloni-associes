a0f0eba402f3c458b521ee0d2701cad6a7251d06
Chaloulou75 <c.jeandey@gmail.com>
2020-11-02 16:37:44 -0300

    px contact

--- components/Contact.vue
+++ components/Contact.vue
@@ -5,7 +5,7 @@
       <div class="justify-around lg:flex">
 
         <div class="flex justify-start w-full px-2 text-gray-100 lg:px:8 lg:justify-center lg:w-1/3">
-          <div class="px-12 py-4 rounded-lg">
+          <div class="px-2 py-4 rounded-lg lg:px-12">
               <p class="flex items-center text-lg lg:text-xl">
                 <svg class="inline-block w-6 h-6 mr-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path></svg>
                 Adresse:
@@ -18,7 +18,7 @@
         </div>
 
         <div class="flex justify-start w-full px-2 text-gray-100 lg:justify-center lg:w-1/3">
-          <div class="px-12 py-4 rounded-lg lg:my-0">
+          <div class="px-2 py-4 rounded-lg lg:px-12 lg:my-0">
             <p class="flex items-center pb-4 text-base text-center text-gray-100 cursor-pointer hover:font-semibold lg:text-xl">
               <svg class="inline-block w-6 h-6 mr-4 lg:mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"></path></svg>
               <a href="mailto:mpantaloni@pantaloni-avocats.com" class="transition duration-500 ease-in-out transform text-gold hover:text-gray-100 hover:translate-x-2">mpantaloni@pantaloni-avocats.com</a>
@@ -27,7 +27,7 @@
         </div>
 
         <div class="flex justify-start w-full px-2 text-gray-100 lg:px:8 lg:justify-center lg:w-1/3">
-          <div class="px-12 py-4 rounded-lg lg:my-0">
+          <div class="px-2 py-4 rounded-lg lg:px-12 lg:my-0">
             <p class="flex items-center text-base lg:text-lg lg:my-0"><svg class="inline-block w-6 h-6 mr-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"></path></svg> + 33 1 85 73 40 50</p>
             <p class="flex items-center mt-2 text-base lg:text-lg">
               <span class="p-1 mr-2 font-semibold"> Fax: </span> + 33 1 86 95 29 57
--- pages/index.vue
+++ pages/index.vue
@@ -6,13 +6,15 @@
       <Equipe />
       <Contact />
       <Footer />
-      <div id="myBtn" class="fixed bottom-0 right-0 z-20 mb-12 mr-5">
+
+      <div class="fixed bottom-0 right-0 z-20 mb-12 mr-5">
         <div class="p-2 bg-gray-100 border rounded-full opacity-50">
           <scroll-link href="#herotop" class="text-gray-800">
             <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 11l3-3m0 0l3 3m-3-3v8m0-13a9 9 0 110 18 9 9 0 010-18z"></path></svg>
           </scroll-link>
         </div>
       </div>
+
   </div>
 </template>
 
