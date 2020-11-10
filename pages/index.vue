5dc6b907ec536cf6174015866d5dc3d236599c49
Chaloulou75 <c.jeandey@gmail.com>
2020-11-10 13:43:10 -0300

    button in-viewport

--- pages/index.vue
+++ pages/index.vue
@@ -7,13 +7,15 @@
       <Contact />
       <Footer />
 
-      <div class="fixed bottom-0 right-0 z-20 mb-12 mr-5">
-        <div class="p-2 bg-gray-100 border rounded-full opacity-50">
-          <scroll-link href="#herotop" class="text-gray-800">
-            <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 11l3-3m0 0l3 3m-3-3v8m0-13a9 9 0 110 18 9 9 0 010-18z"></path></svg>
-          </scroll-link>
+      <visible when-hidden="#herotop">
+        <div class="fixed bottom-0 right-0 z-20 mb-12 mr-5">
+          <div class="p-2 bg-gray-300 border rounded-full opacity-75">
+            <scroll-link href="#herotop" class="text-gray-800">
+              <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 11l3-3m0 0l3 3m-3-3v8m0-13a9 9 0 110 18 9 9 0 010-18z"></path></svg>
+            </scroll-link>
+          </div>
         </div>
-      </div>
+      </visible>
 
   </div>
 </template>
@@ -25,6 +27,8 @@ import Equipe from './../components/Equipe.vue'
 import Contact from './../components/Contact.vue'
 import Footer from './../components/Footer.vue'
 import ScrollLink from './../components/ScrollLink.vue'
+import Visible from './../components/Visible.vue'
+
 
 export default {
   components :{
@@ -33,7 +37,8 @@ export default {
     Equipe,
     Contact,
     Footer,
-    ScrollLink
+    ScrollLink,
+    Visible
   },
 }
 </script>
