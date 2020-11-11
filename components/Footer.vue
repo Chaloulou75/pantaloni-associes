cfc37cd0284d2b47610e0794efbb40f921b234a5
Chaloulou75 <c.jeandey@gmail.com>
2020-11-10 22:32:08 -0300

    overflow-x

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,9 +1,13 @@
 <template>
-<div class="pt-2 bg-gray-800 ">
-   <div class="flex items-center justify-between max-w-6xl px-3 py-5 m-auto text-xs text-gray-200 lg:text-sm md:flex-row">
-      <div >&copy; {{ new Date().getFullYear() }}. Tous droits réservés.</div>
+<div class="pt-4 bg-gray-800 ">
+   <div class="flex flex-col items-center justify-between w-full px-3 py-5 m-auto text-xs text-gray-200 lg:text-sm md:flex-row">
+      <div >&copy; Pantaloni & Associés {{ new Date().getFullYear() }}. Tous droits réservés.</div>
 
-      <NuxtLink to="/mentionslegales" class="hover:text-gray-300">Mentions légales</NuxtLink>
+      <div class="flex justify-between">
+        <p class="pr-6">Developpeur Web: <a href="https://charles-jeandey.com" target="_blank" class="px-1 text-gray-400 hover:text-gray-200"> Charles Jeandey</a></p>
+        <NuxtLink to="/mentionslegales" class="hover:text-gray-300">Mentions légales</NuxtLink>
+      </div>
+
 
    </div>
 </div>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="relative">
+  <div class="relative overflow-hidden">
       <Hero />
       <Cabinet />
       <!-- <Expertise /> -->
@@ -8,7 +8,7 @@
       <Footer />
 
       <visible when-hidden="#herotop">
-        <div class="fixed bottom-0 right-0 z-20 mb-12 mr-5">
+        <div class="fixed bottom-0 right-0 z-20 mb-16 mr-6">
           <div class="p-2 bg-gray-300 border rounded-full opacity-75">
             <scroll-link href="#herotop" class="text-gray-800">
               <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 11l3-3m0 0l3 3m-3-3v8m0-13a9 9 0 110 18 9 9 0 010-18z"></path></svg>
